f5f961afb9d25d5740baa3c69963fba078fd9e9c
Patrick Perosa <patrickperosapp@gmail.com>
2021-07-15 13:13:17 -0400

    save

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import AppProvider from './src/contexts'
+import Routes from './src/routes'
+import { theme } from './src/styles/theme'
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <Text> Salve</Text>
-    </SafeAreaView>
-  );
-};
+    return (
+        <ThemeProvider theme={theme}>
+            <NavigationContainer>
+                <AppProvider>
+                    <Routes />
+                </AppProvider>
+            </NavigationContainer>
+        </ThemeProvider>
+    )
+}
 
-export default App;
+export default App
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,162 +1,183 @@
-const baseUrl = 'https://api.b7web.com.br/devcond/api/admin'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const baseUrl = 'https://api.b7web.com.br/devcond/api'
+
+var token = AsyncStorage.getItem('@devcond:token')
 
 const request = async (
-  method: any,
-  endpoint: any,
-  params: any,
-  token: any = null
+    method: any,
+    endpoint: any,
+    params: any,
+    token: any = null
 ) => {
-  method = method.toLowerCase()
-  let fullUrl = `${baseUrl}${endpoint}`
-  let body = null
+    method = method.toLowerCase()
+    let fullUrl = `${baseUrl}${endpoint}`
+    let body = null
 
-  switch (method) {
-    case 'get':
-      let queryString = new URLSearchParams(params).toString()
-      fullUrl += `?${queryString}`
-      break
-    case 'post':
-    case 'put':
-    case 'delete':
-      body = JSON.stringify(params)
-      break
-    default:
-      break
-  }
+    switch (method) {
+        case 'get':
+            let queryString = new URLSearchParams(params).toString()
+            fullUrl += `?${queryString}`
+            break
+        case 'post':
+        case 'put':
+        case 'delete':
+            body = JSON.stringify(params)
+            break
+        default:
+            break
+    }
 
-  let headers: any = { 'Content-Type': 'application/json' }
+    let headers: any = { 'Content-Type': 'application/json' }
 
-  if (token) {
-    headers.Authorization = `Bearer ${token}`
-  }
+    if (token) {
+        headers.Authorization = `Bearer ${token}`
+    }
 
-  let req = await fetch(fullUrl, { method, headers, body })
-  let json = await req.json()
+    let req = await fetch(fullUrl, { method, headers, body })
+    let json = await req.json()
 
-  return json
+    return json
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
-  var token = localStorage.getItem('@token/conddev')
-  return {
-    getToken: () => {
-      return localStorage.getItem('@token/conddev')
+export default {
+    getToken: async () => {
+        return await AsyncStorage.getItem('@devcond:token')
     },
 
     validadeToken: async () => {
-      let json = await request('post', '/auth/validate', {}, token)
-      return json
+        let json = await request('post', '/auth/validate', {}, token)
+        return json
     },
 
-    login: async (email: string, password: string) => {
-      let json = await request('post', '/auth/login', { email, password })
-      return json
+    login: async (cpf: string, password: string) => {
+        let json = await request('post', '/auth/login', { cpf, password })
+        return json
     },
     logout: async () => {
-      let json = await request('post', '/auth/logout', {}, token)
-      localStorage.removeItem('@token/conddev')
-      return json
+        let json = await request('post', '/auth/logout', {}, token)
+        await AsyncStorage.removeItem('@devcond:token')
+        await AsyncStorage.removeItem('@devcond:property')
+
+        return json
     },
 
+    register: async (
+        name: string,
+        cpf: string,
+        email: string,
+        password: string,
+        password_confirm: string
+    ) => {
+        let json = await request('post', '/auth/register', {
+            name,
+            email,
+            cpf,
+            password,
+            password_confirm
+        })
+        return json
+    },
     getWall: async () => {
-      let json = await request('get', '/walls', {}, token)
-      return json
+        let token = AsyncStorage.getItem('@devcond:token')
+        console.log(token)
+
+        let json = await request('get', '/walls', {}, token)
+        return json
     },
     updateWall: async (id: string, data: { title: string; body: string }) => {
-      let json = await request('put', `/wall/${id}`, data, token)
-      return json
+        let json = await request('put', `/wall/${id}`, data, token)
+        return json
     },
     addWall: async (data: { title: string; body: string }) => {
-      let json = await request('post', `/walls`, data, token)
-      return json
+        let json = await request('post', `/walls`, data, token)
+        return json
     },
 
     removeWall: async (id: string) => {
-      let json = await request('delete', `/wall/${id}`, {}, token)
-      return json
+        let json = await request('delete', `/wall/${id}`, {}, token)
+        return json
     },
     getDocuments: async () => {
-      let json = await request('get', `/docs`, {}, token)
-      return json
+        let json = await request('get', `/docs`, {}, token)
+        return json
     },
-    addDocument: async (data: { title: string; file: File }) => {
-      let formData = new FormData()
-      formData.append('title', data.title)
-      if (data.file) {
-        formData.append('file', data.file)
-      }
+    addDocument: async (data: { title: string; file: any }) => {
+        let formData = new FormData()
+        formData.append('title', data.title)
+        if (data.file) {
+            formData.append('file', data.file)
+        }
 
-      let req = await fetch(`${baseUrl}/docs`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        body: formData
-      })
+        let req = await fetch(`${baseUrl}/docs`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: formData
+        })
 
-      let json = await req.json()
+        let json = await req.json()
 
-      return json
+        return json
     },
-    updateDocument: async (id: string, data: { title: string; file: File }) => {
-      let formData = new FormData()
-      formData.append('title', data.title)
-      if (data.file) {
-        formData.append('file', data.file)
-      }
+    updateDocument: async (id: string, data: { title: string; file: any }) => {
+        let formData = new FormData()
+        formData.append('title', data.title)
+        if (data.file) {
+            formData.append('file', data.file)
+        }
 
-      let req = await fetch(`${baseUrl}/doc/${id}`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        body: formData
-      })
+        let req = await fetch(`${baseUrl}/doc/${id}`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: formData
+        })
 
-      let json = await req.json()
+        let json = await req.json()
 
-      return json
+        return json
     },
 
     removeDocument: async (id: string) => {
-      let json = await request('delete', `/doc/${id}`, {}, token)
-      return json
+        let json = await request('delete', `/doc/${id}`, {}, token)
+        return json
     },
     getReservations: async () => {
-      let json = await request('get', `/reservations`, {}, token)
-      return json
+        let json = await request('get', `/reservations`, {}, token)
+        return json
     },
     getUnits: async () => {
-      let json = await request('get', `/units`, {}, token)
-      return json
+        let json = await request('get', `/units`, {}, token)
+        return json
     },
     getAreas: async () => {
-      let json = await request('get', `/areas`, {}, token)
-      return json
+        let json = await request('get', `/areas`, {}, token)
+        return json
     },
     addReservation: async (data: {
-      id_unit: number
-      id_area: number
-      reservation_date: string
-    }) => {
-      let json = await request('post', `/reservations`, data, token)
-      return json
-    },
-    updateReservation: async (
-      id: string,
-      data: {
         id_unit: number
         id_area: number
         reservation_date: string
-      }
+    }) => {
+        let json = await request('post', `/reservations`, data, token)
+        return json
+    },
+    updateReservation: async (
+        id: string,
+        data: {
+            id_unit: number
+            id_area: number
+            reservation_date: string
+        }
     ) => {
-      let json = await request('put', `/reservation/${id}`, data, token)
-      return json
+        let json = await request('put', `/reservation/${id}`, data, token)
+        return json
     },
     removeReservation: async (id: string) => {
-      let json = await request('delete', `/reservation/${id}`, {}, token)
-      return json
+        let json = await request('delete', `/reservation/${id}`, {}, token)
+        return json
     }
-  }
 }
